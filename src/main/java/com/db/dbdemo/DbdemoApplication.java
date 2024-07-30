b79281f5eda564cc19b126424f1e28231cba2c74
Admin <khalidbutt@live.de>
2024-07-30 03:22:57 -0700

    init

--- src/main/java/com/db/dbdemo/DbdemoApplication.java
+++ src/main/java/com/db/dbdemo/DbdemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DbdemoApplication {
+public class DbdemoApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbdemoApplication.class, args);
